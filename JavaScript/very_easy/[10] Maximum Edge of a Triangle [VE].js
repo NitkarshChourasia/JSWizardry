b071acdfb31be0e9575a91441c72229fd53f144b
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-02 14:53:14 +0530

    Javascript, very_easy, solving [10].

--- JavaScript/very_easy/[10] Maximum Edge of a Triangle [VE].js
+++ JavaScript/very_easy/[10] Maximum Edge of a Triangle [VE].js
@@ -66,3 +66,13 @@ _________
 */
 //Your code should go here:
 
+function nextEdge(side1, side2) {
+    return (side1 + side2) - 1;
+}
+
+
+if (require.main == module) {
+    console.log(nextEdge(8, 10))
+    console.log(nextEdge(5, 7))
+    console.log(nextEdge(9, 2))
+}
