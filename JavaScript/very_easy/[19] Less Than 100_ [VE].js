be3b5936be7b24ab270dac909e2707d163c80f65
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-03 16:13:03 +0530

    Javascript, very_easy, solving [19].

--- JavaScript/very_easy/[19] Less Than 100_ [VE].js
+++ JavaScript/very_easy/[19] Less Than 100_ [VE].js
@@ -8,10 +8,10 @@ Given two numbers, return true if the sum of both numbers is less than 100. Othe
 
 ___
 lessThan100(22, 15) ➞ true
-// 22 + 15 = 37
+-> 22 + 15 = 37
 
 lessThan100(83, 34) ➞ false
-// 83 + 34 = 117
+ -> 83 + 34 = 117
 
 lessThan100(3, 77) ➞ true
 _____
@@ -78,3 +78,14 @@ _________
 */
 //Your code should go here:
 
+function lessThan100(input_number1, input_number2){
+    return input_number1 + input_number2 < 100
+}
+
+if (require.main == module){
+    console.log(lessThan100(22, 15))
+    console.log(lessThan100(83, 34))
+    console.log(lessThan100(3, 77))
+}
+
+// complete.
