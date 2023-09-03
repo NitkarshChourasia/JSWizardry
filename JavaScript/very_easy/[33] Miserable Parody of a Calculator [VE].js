e70eb3b0af73d93cd2f5642bed0d6102398f9ee1
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-03 23:54:59 +0530

    Javascript, very_easy, solving [33].

--- JavaScript/very_easy/[33] Miserable Parody of a Calculator [VE].js
+++ JavaScript/very_easy/[33] Miserable Parody of a Calculator [VE].js
@@ -63,3 +63,15 @@ _________
 */
 //Your code should go here:
 
+function calculator(str) {
+    return eval(str);
+}
+
+if (require.main == module) {
+    console.log(calculator("23+4"))
+    console.log(calculator("45-15"))
+    console.log(calculator("13+2-5*2"))
+    console.log(calculator("49/7*2-3"))
+}
+
+// complete.
