34e5ac405d6b09e511a56c732236cc1894017fb6
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-03 23:09:42 +0530

    Javascript, very_easy, solving [24].

--- JavaScript/very_easy/[24] Are the Numbers Equal_ [VE].js
+++ JavaScript/very_easy/[24] Are the Numbers Equal_ [VE].js
@@ -51,3 +51,17 @@ _________
 */
 //Your code should go here:
 
+
+function isSameNum(num1, num2){
+    return num1 === num2
+}
+
+
+if (require.main == module)
+{
+    console.log(isSameNum(4, 8))
+    console.log(isSameNum(2, 2))
+    console.log(isSameNum(2, "2"))
+}
+
+// complete.
