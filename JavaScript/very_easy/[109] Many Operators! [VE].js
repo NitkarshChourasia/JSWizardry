c30ac2e9b33d9c68e949a9549b7682bfe1c95dab
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-12 13:24:11 +0530

    Javascript, very_easy, calculator used eval and switch, [109].

--- JavaScript/very_easy/[109] Many Operators! [VE].js
+++ JavaScript/very_easy/[109] Many Operators! [VE].js
@@ -8,13 +8,13 @@ Some basic arithmetic operators are +, -, *, /, and %. In this challenge you wil
 
 ___
 operate(1, 2, "+") ➞ 3
-// 1 + 2 = 3
+1 + 2 = 3
 
 operate(7, 10, "-") ➞ -3
-// 7 - 10 = -3
+7 - 10 = -3
 
 operate(20, 10, "%") ➞ 0
-// 20 % 10 = 0
+20 % 10 = 0
 _____
 
 
@@ -24,7 +24,7 @@ _____
 There will not be any divisions by zero.
 
 
-[language_fundamentals] [math] 
+[language_fundamentals] [math]
 
 
 
@@ -64,3 +64,48 @@ _________
 */
 //Your code should go here:
 
+// The input will be string, can it be used directly by somehow converting it into operator something.
+// Options are eval.
+// Or switch case.
+// if else if else.
+// What and how to convert input num into any kind base num.
+
+
+function operate(num1, num2, operator) {
+    return eval(num1 + operator + num2);
+} // The worst harmful method.
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            if num2 == 0 {
+                return "Divide by zero error.";
+            }
+            return num1 / num2;
+        case "%":
+            return num1 % num2;
+        default:
+            return "Invalid operator.";
+    }
+} // The best method.
+
+if (require.main == module) {
+    // Test cases.
+    console.log(operate(1, 2, "+"));
+    console.log(operate(7, 10, "-"));
+    console.log(operate(20, 10, "%"));
+    // Custom test cases.
+    console.log(operate(20, 0, "/"));
+    console.log(operate(20, 10, "/"));
+    console.log(operate(20, 10, "*"));
+    console.log(operate(20, 10, "a"));
+}
+
+// 12-09-2023
+// complete.
