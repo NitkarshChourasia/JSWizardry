1a24c95ca56e84c278396b8527a9251901b2c542
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-12 15:39:33 +0530

    Javascript, very_easy, return arr typeof each ele of given arr, [114].

--- JavaScript/very_easy/[114] Return Types [VE].js
+++ JavaScript/very_easy/[114] Return Types [VE].js
@@ -70,5 +70,4 @@ if (require.main == module) {
     console.log(arrayValuesTypes([1, 2, "null", []]));
     console.log(arrayValuesTypes(["214", true, false, 2, 2.15, [], null]));
     console.log(arrayValuesTypes([21.1, "float", "array", ["I am array"], null, true, 214]));
-
 }
