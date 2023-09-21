58515d35ea98fbee4a5f90c4bf04a76958d742ea
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-09-21 16:14:52 +0530

    Javascript, very_easy, Correcting the outputs and stamping date, [135].

--- JavaScript/very_easy/[135] First Class_ Second Class and Third Class Levers [VE].js
+++ JavaScript/very_easy/[135] First Class_ Second Class and Third Class Levers [VE].js
@@ -54,3 +54,80 @@ _________
 */
 //Your code should go here:
 
+"use strict";
+
+// Method 1 - Using if else if else statement.
+function determineLeverIfElse(arr) {
+    if (arr[1] === "f") {
+        return "first class lever";
+    } else if (arr[2] === "f") {
+        return "second class lever";
+    } else {
+        return "third class lever";
+    }
+}
+
+
+// Method 2 - Using ternary operator in function.
+function determineLeverTernary(arr) {
+    return arr[1] === "f" ? "first class lever" : arr[2] === "f" ? "second class lever" : "third class lever";
+}
+
+// Method 3 - Using ternary operator with indexOf() method in function.
+function determineLeverTernaryIndexOf(arr) {
+    return arr.indexOf("f") === 1 ? "first class lever" : arr.indexOf("f") === 2 ? "second class lever" : "third class lever";
+}
+
+function determineLever(arr) {
+    let arrStr = arr.join("").toLowerCase();
+    const leverTypeObj = { efl: "first class lever", elf: "second class lever", fel: "third class lever" };
+    return leverTypeObj[arrStr];
+} // Best way in my opinion.
+
+if (require.main == module) {
+    console.log("#### First Class, Second Class and Third Class Levers  ####\n");
+    // Test cases.
+
+    // Method 1 - Using if else if else statement.
+    console.log("Method 1 - Using if else if else statement.");
+    console.log(determineLeverIfElse(["e", "f", "l"]));
+    console.log(determineLeverIfElse(["e", "l", "f"]));
+    console.log(determineLeverIfElse(["f", "e", "l"]));
+    console.log(); // Blank line for readability.
+
+    // Method 2 - Using ternary operator in function.
+    console.log("Method 2 - Using ternary operator in function.");
+    console.log(determineLeverTernary(["e", "f", "l"]));
+    console.log(determineLeverTernary(["e", "l", "f"]));
+    console.log(determineLeverTernary(["f", "e", "l"]));
+    console.log(); // Blank line for readability.
+
+    // Method 3 - Using ternary operator with indexOf() method in function.
+    console.log("Method 3 - Using ternary operator with indexOf() method in function.");
+    console.log(determineLeverTernaryIndexOf(["e", "f", "l"]));
+    console.log(determineLeverTernaryIndexOf(["e", "l", "f"]));
+    console.log(determineLeverTernaryIndexOf(["f", "e", "l"]));
+    console.log(); // Blank line for readability.
+
+
+
+    // Method 4 - Using object and join() method in function for result output, the best way, here, in my opinion.
+    console.log("Method 4 - Using obj and .join() method for result output.");
+    console.log(determineLever(["e", "f", "l"]));
+    console.log(determineLever(["e", "l", "f"]));
+    console.log(determineLever(["f", "e", "l"]));
+    console.log(); // Blank line for readability.
+
+    // console.log(); // Blank line for readability.
+    // Custom test cases.
+
+    // Method 4 - Using object and join() method in function for result output, the best way, here, in my opinion.
+    console.log("Custom test cases:\n"); // Blank line for readability.
+    console.log("Method 4 - Using obj and .join() method for result output.");
+    console.log(determineLever(["E", "F", "L"]));
+    console.log(determineLever(["E", "L", "F"]));
+    console.log(determineLever(["F", "E", "L"]));
+}
+
+// 21-09-2023
+// complete.
--- JavaScript/very_easy/tempCodeRunnerFile.js
+++ JavaScript/very_easy/tempCodeRunnerFile.js
@@ -1,1 +1,22 @@
+function determineLever(arr) {
+    arrStr = arr.join("").toLowerCase();
+    leverTypeObj = { efl: "first class lever", elf: "second class lever", fel: "third class lever" };
+    return leverTypeObj[arrStr];
+} // Best way in my opinion.
 
+if (require.main == module) {
+    console.log("#### First Class, Second Class and Third Class Levers  ####\n");
+    // Test cases.
+    console.log(determineLever(["e", "f", "l"]));
+    console.log(determineLever(["e", "l", "f"]));
+    console.log(determineLever(["f", "e", "l"]));
+
+    console.log(); // Blank line for readability.
+    // Custom test cases.
+    console.log(determineLever(["E", "F", "L"]));
+    console.log(determineLever(["E", "L", "F"]));
+    console.log(determineLever(["F", "E", "L"]));
+}
+
+// 18-09-2023
+// complete.
